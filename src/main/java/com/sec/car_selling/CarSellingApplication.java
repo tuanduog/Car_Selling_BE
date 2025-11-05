4eb863dd66d1feeb8a8b8af99133d4eb19af36f4
tuanduog <tuanduog04@gmail.com>
2025-11-05 10:56:08 +0700

    init

---
+++ src/main/java/com/sec/car_selling/CarSellingApplication.java
@@ -0,0 +1,13 @@
+package com.sec.car_selling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSellingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarSellingApplication.class, args);
+	}
+
+}
